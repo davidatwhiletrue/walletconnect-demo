0a0193eff5eec764e9d49107ca73829804400d24
David Hernando <david.hernando@make.services>
2024-01-29 11:22:09 +0100

    template with wallet connect

--- src/index.tsx
+++ src/index.tsx
@@ -9,8 +9,13 @@ import App from './App';
 const clickOptions: CsprClickInitOptions = {
 	appName: 'CSPR.app',
 	contentMode: CONTENT_MODE.IFRAME,
-	providers: ['casper-wallet', 'ledger', 'torus-wallet', 'casperdash', 'metamask-snap', 'casper-signer'],
-	appId: 'csprclick-template',
+	providers: ['casper-wallet', 'ledger', 'walletconnect', 'casperdash', 'metamask-snap', 'torus-wallet', 'casper-signer'],
+	appId: 'csprclick-template',    
+    walletConnect: {
+        logger: 'debug',
+        relayUrl: 'wss://relay.walletconnect.com',
+        projectId: '6cdf3095d26e511f0126b484e219cc4d',
+    }
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
